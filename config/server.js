0b17fd068eba730bdec88b1b705c42f5aeaabf00
baroxdev <92500416+baroxdev@users.noreply.github.com>
2023-12-02 20:28:33 +0700

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0e2b21b280ca39761628fc237c2631b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '49db1043-92cb-43fb-9198-e3ce68fd4028'
+};
